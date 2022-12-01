302fc8d66aa8c74376c88030b713f2956549db31
yonatandatamaster <102936017+yonatandatamaster@users.noreply.github.com>
2022-12-01 21:11:22 +0700

    Update 1_DSO.py

--- 1_DSO.py
+++ 1_DSO.py
@@ -11,7 +11,7 @@ st.sidebar.title('Live Dashboard Pencapaian Program')
 
 import pandas as pd
 import numpy as np
-
+import matplotlib.pyplot as plt
 
 import plotly.express as px
 from plotly.subplots import make_subplots
