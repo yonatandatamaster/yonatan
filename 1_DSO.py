f4415ca077f5f58b1167fbd516f2daeef6d335f4
yonatandatamaster <102936017+yonatandatamaster@users.noreply.github.com>
2022-12-03 09:02:27 +0700

    Update 1_DSO.py

--- 1_DSO.py
+++ 1_DSO.py
@@ -2,8 +2,7 @@ import streamlit as st
 
 
 st.set_page_config(page_title="Multipage App",
-    page_icon=":bar_chart:",
-    layout="wide")
+    page_icon=":bar_chart:", layout='wide')
 
 st.title('Live Dashboard Program Kampus A🤖')
 st.sidebar.title('Live Dashboard Pencapaian Program')
@@ -11,19 +10,19 @@ st.sidebar.title('Live Dashboard Pencapaian Program')
 
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
+
 
 import plotly.express as px
 from plotly.subplots import make_subplots
 import plotly.graph_objects as go
 
-
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 
 df = pd.read_excel('Data Konsumen A_46_KONSUMEN.xlsx',skiprows = 2)
 df.drop(['Unnamed: 0','HP','Harga','Unnamed: 8','Unnamed: 9'],axis = 1,inplace = True)
 
-grouped_1 = df[['Bumo','Profesi','Usia']].groupby(['Bumo'],as_index = False).mean()
 
 bumo_count = df['Bumo'].value_counts()
 bumo_count = pd.DataFrame({'BUMO':bumo_count.index,'Jumlah Konsumen':bumo_count.values})
@@ -32,15 +31,17 @@ st.sidebar.markdown('### BUMO Konsumen')
 select = st.sidebar.selectbox('Pilih Jenis Visualisasi',['Bar Chart','Pie Chart'],key='1')
 
 if not st.sidebar.checkbox('Hide',True):
-    
     if select == 'Bar Chart':
-        st.markdown('Jumlah Konsumen by BUMO (All Brands)')
+        st.markdown('#### Jumlah Konsumen by BUMO (All Brands)')
         fig = px.bar(bumo_count, x= 'BUMO',y = 'Jumlah Konsumen',color = 'BUMO',height = 500)
         st.plotly_chart(fig)
+       
     else:
-        st.markdown('Jumlah Konsumen by BUMO (Top 8 Only)')
-        fig = px.pie(bumo_count.head(8),names = 'BUMO',values = 'Jumlah Konsumen',)
+        st.markdown('Jumlah Konsumen by BUMO (Top 10 Only)')
+        fig = px.pie(bumo_count.sample(n=10),names = 'BUMO',values = 'Jumlah Konsumen',width=400)
         st.plotly_chart(fig)
+     
+        
 
 
 df_2 = pd.read_excel('Data Konsumen A_46_KONSUMEN.xlsx',sheet_name = 'DATA_2')
@@ -53,18 +54,17 @@ if not st.sidebar.checkbox('Hide',True, key = '10'):
     
     if select_2 == 'Outlet Terdata vs Outlet Beli':
         st.markdown('#### Pencapaian Target Outlet')
-        fig = px.bar(df_2,x='Minggu', y=['O_BELI','O_TERDATA'],barmode = 'group')
+        fig = px.bar(df_2,x='Minggu', y=['O_BELI','O_TERDATA'],barmode = 'group',height=500)
         st.plotly_chart(fig)
     else:
-        st.markdown('#### Pencapaian Target Scan')
-        fig = px.bar(df_2,x = 'Minggu',y =['TOT_SCAN','SE_LAB20'],barmode = 'group')
+        st.markdown('#### Pencapaian Target Scan (%)')
+        fig = px.bar(df_2,x = 'Minggu',y =['TOT_SCAN','SE_LAB20'],barmode = 'group',height=500)
         fig_2 = px.line(df_2, x ='Minggu',y='Change')
         fig_2.update_layout(template='gridon')
         st.plotly_chart(fig)
         st.plotly_chart(fig_2)
 
-
-df_3 = pd.read_excel('Data Konsumen A_46_KONSUMEN.xlsx',sheet_name = 'DATA_3')
+df_3 = pd.read_excel(r'C:\Users\yonat\Coursera\Djarum, 2022, Semester 2\Program Kampus\Data Konsumen A_46_KONSUMEN.xlsx',sheet_name = 'DATA_3')
 
 df_3.columns = ['DSO', 'ID_Outlet', 'Nama_Outlet', 'Alamat', 'Area_Kampus', 'Promotor', 'Program', 'Minggu', 'Brand', 'Pembelian_(bks)', 'latitude', 'longitude']
 
@@ -81,6 +81,14 @@ del_val = ['D Coklat 12','D Super 50']
 df_3 = df_3[df_3.Brand.isin(del_val)==False]
 
 
+# In case need groupby and pivot:
+#grpd_3 = df_3[['DSO','Program','Brand','Pembelian_(bks)']]
+#gby_3 = grpd_3.groupby(['DSO','Brand'],as_index = False).sum()
+#pvt_3 = gby_3.pivot(index='DSO',columns = 'Brand')
+#pvt_3.columns = pvt_3.columns.droplevel(0)
+# -------------------------------
+
+
 st.sidebar.subheader('Omset by DSO')
 choice = st.sidebar.multiselect('Pilih DSO nya:',('Depok','Jakarta Barat','Jakarta Selatan','Jakarta Timur'))
 
@@ -92,47 +100,83 @@ if len(choice)>0:
     fig_choice = px.histogram(choice_data, x = 'Brand',y='Pembelian_(bks)', histfunc='sum',facet_col='DSO',color='Brand',labels={'DSO':'Omset'},height=600,width=800)
     st.plotly_chart(fig_choice)
     fig_choice_2 = px.line(choice_data,x='Minggu',y='Pembelian_(bks)')
-    choice_2 = st.slider('Pilih Minggu',0,5)
+
 
 st.sidebar.markdown('---')
 st.sidebar.markdown('---')
 
-min_ = df_3['Minggu'].min()
-
-print(type(min_))
-listt = [1,2,3,4,5,5,5,5]
-
-huhu = df_3['Minggu'].values
 
 ## print(np.unique(df_3['Minggu'].values.tolist()))
+# --------------------------------------------------------------
+# --------------------------------------------------------------
 
-sheet_id = '1UgUrsuKOpxROHkvraJVdHvcWMRZplLShbiTP_izNTnM'
+# Data Depok A
+sheet_id_depok_a = '1LGliLdr2teOiY4QlRhOx73dkJ5JunhvVfiPHrDvx4Uk'
 
-xls = pd.ExcelFile(f"https://docs.google.com/spreadsheets/d/{sheet_id}/export?format=xlsx")
-dfdf = pd.read_excel(xls)
-dfdf.columns = ['Timestamp', 'Email', 'Kode',
-       'Nama', 'Usia',
-       'Profesi_notfix',
-       'No_HP',
-       'BUMO',
+xls_depok_a = pd.ExcelFile(f"https://docs.google.com/spreadsheets/d/{sheet_id_depok_a}/export?format=xlsx")
+df_depok_a = pd.read_excel(xls_depok_a,skiprows=1982)
+df_depok_a.columns = ['Timestamp', 'Nama', 'Usia',
+       'No HP', 'Profesi_notfix',
+       'Kode',
+       'SOA',
+       'email',
        'Profesi',
+       'BUMO']
+
+df_depok_a = df_depok_a.loc[:,['Timestamp', 'email', 'Kode',
+       'Nama', 'Usia',
+       'Profesi',
+       'No HP',
+       'BUMO',
+       'Profesi_notfix',
+       'SOA']]
+
+# --------------------------------------------------------------
+
+# Data Jakarta Barat A
+sheet_id_jakbar_a = '1dvkWPfxFF_jB3YlCN5tsQCmuiXkLuZOGeb2W6ffLS5k'
+
+xls_jakbar_a = pd.ExcelFile(f"https://docs.google.com/spreadsheets/d/{sheet_id_jakbar_a}/export?format=xlsx")
+df_jakbar_a = pd.read_excel(xls_jakbar_a,skiprows = 1304 )
+df_jakbar_a.columns = ['Timestamp', 'email', 'Kode',
+       'Nama', 'Usia',
+       'Profesi',
+       'No HP',
+       'BUMO',
+       'Profesi_notfix',
+       'SOA']
+# --------------------------------------------------------------
+
+# Data Jakarta Selatan A
+sheet_id_jaksel_a = '1C3OlhfRdH2KdqUwME2jqedHx33oKFFR8nE0g-aMNsn4'
+
+xls_jaksel_a = pd.ExcelFile(f"https://docs.google.com/spreadsheets/d/{sheet_id_jaksel_a}/export?format=xlsx")
+df_jaksel_a = pd.read_excel(xls_jaksel_a,skiprows = 697 )
+df_jaksel_a.columns = ['Timestamp', 'email', 'Kode',
+       'Nama', 'Usia',
+       'Profesi',
+       'No HP',
+       'BUMO',
+       'Profesi_notfix',
        'SOA']
 
+# --------------------------------------------------------------
+
+### Data Append A
+
+data_append_a = pd.concat([df_depok_a,df_jakbar_a, df_jaksel_a],ignore_index = True)
+
+col1, col2, col3 = st.columns(3)
+with col1:
+    st.header('ABCDE')
+    st.selectbox('Pilih ya',('Kampus A','Kampus B'))
+with col2:
+     fig = px.line(data_append_a,x = 'BUMO',y = 'Usia',)
+     st.plotly_chart(fig)
 
 
-datadata = dfdf[['BUMO','Profesi']]
-
-fig = px.histogram(dfdf[dfdf['Profesi']=="Mahasiswa"],x='BUMO',y = 'Usia',histfunc='avg',color='BUMO')
-st.plotly_chart(fig)
 
 
 
-
-
-
-
-
-
-
-
-
+#fig = px.histogram([data_append_a['Profesi']=="mahasiswa"],x='BUMO',y = 'Usia',histfunc='avg')
+#st.plotly_chart(fig)
