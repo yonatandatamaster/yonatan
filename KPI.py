a67eee8470991c030325cd03a8de46212eca0795
yonatandatamaster <yonatanah.8@gmail.com>
2023-01-09 11:09:37 +0700

    Update KPI.py

--- KPI.py
+++ KPI.py
@@ -350,7 +350,6 @@ kam_a_cha = load_kam_a_table()
 
 fig4 = px.bar(kam_a_tab, x = 'Promotor', y ='% AVG KPI',height =300, width = 450).update_layout(yaxis_ticksuffix = '% Aktif vs Target')
 
-st.header('Program AMU Kampus A')
 st.markdown('Perhitungan persentase 👇 dihitung dari rata-rata Outlet Aktif per Minggu dibagi dengan Target Outlet')
 st.plotly_chart(fig4)
 
