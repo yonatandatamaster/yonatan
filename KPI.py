b95fb0327bb03b4d2dfd26345b78487be4741d8e
yonatandatamaster <yonatanah.8@gmail.com>
2023-01-09 13:05:49 +0700

    Update KPI.py

--- KPI.py
+++ KPI.py
@@ -566,7 +566,7 @@ def final_kpi():
     #merge5.replace(np.nan, 0,inplace = True)
     merge5['AVG Final % KPI'] = merge5.mean(axis = 1,numeric_only = True)
     merge5.columns = ['Promotor','% AMU Sekolah','% Pemukiman','% Ojol','% Kampus A','% Kampus B','% AVG Final KPI']
-    
+    merge5[['Promotor','% AMU Sekolah','% Pemukiman','% Ojol','% Kampus A','% Kampus B','% AVG Final KPI']] = merge5[['Promotor','% AMU Sekolah','% Pemukiman','% Ojol','% Kampus A','% Kampus B','% AVG Final KPI']].astype(float).round(1) 
     
     return merge5
 
